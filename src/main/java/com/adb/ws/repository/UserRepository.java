6cf02ab9da1fd88cf373577dc4bd39320176c2c3
croshan23 <croshan23@gmail.com>
2018-12-12 23:17:58 -0500

    Features: getAllUsers with Pagination - Implemented getUsers in UserController - Added getUsers method in UserService and UserServiceImpl - Changed the super class in UserRepository from CrudRepo to PaginationAndSortingRepo

--- src/main/java/com/adb/ws/repository/UserRepository.java
+++ src/main/java/com/adb/ws/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.adb.ws.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.adb.ws.io.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> { 
+	// this is used to implement paging and sorting, not provided by CrudRepo
+	// Now we can get methods that will get data based on limits eg. get all 25 items.
+	//CrudRepository<UserEntity, Long> { // was before
 
 	// Spring Data JPA help to write methods to get data from database
 	// Here, find and By is a keyword to search record 
