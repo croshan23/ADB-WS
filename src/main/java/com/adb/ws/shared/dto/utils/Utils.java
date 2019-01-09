e621b9526b436a9af5be7647e1d422ef6709607c
croshan23 <croshan23@gmail.com>
2019-01-08 23:43:28 -0500

    Created method in Utils class to check whether token has expired.

--- src/main/java/com/adb/ws/shared/dto/utils/Utils.java
+++ src/main/java/com/adb/ws/shared/dto/utils/Utils.java
@@ -1,9 +1,15 @@
 package com.adb.ws.shared.dto.utils;
 
 import java.security.SecureRandom;
+import java.util.Date;
 import java.util.Random;
 
 import org.springframework.stereotype.Component;
+
+import com.adb.ws.security.SecurityConstants;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 @Component
 public class Utils {
@@ -27,5 +33,18 @@ public class Utils {
         }
 
         return new String(returnValue);
-    }   
+    }
+
+	public static boolean hasTokenExpired(String token) {
+
+		Claims claims = Jwts.parser()
+				.setSigningKey(SecurityConstants.getTokenSecret())
+				.parseClaimsJws(token).getBody();
+		
+		Date tokenExpirationDate = claims.getExpiration();
+		Date todayDate = new Date();
+		
+		return tokenExpirationDate.before(todayDate);
+	}
+    
 }
